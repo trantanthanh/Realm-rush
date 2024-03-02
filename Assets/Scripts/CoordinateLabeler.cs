b493bf06ce2c30932aa1c466ae0024458776cf5a
Thành Trần Tấn <trantanthanh@gmail.com>
2024-03-02 15:57:55 +0700

    update object name via position on grid

---
+++ Assets/Scripts/CoordinateLabeler.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[ExecuteAlways]
+public class CoordinateLabeler : MonoBehaviour
+{
+    [SerializeField] TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+    void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        DisplayCoordinates();//call once for play mode
+        UpdateObjectName();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            DisplayCoordinates();
+            UpdateObjectName();
+        }
+    }
+
+    private void UpdateObjectName()
+    {
+        transform.parent.name = label.text;
+    }
+
+    private void DisplayCoordinates()
+    {
+        if (label == null) return;
+        coordinates.x = Mathf.RoundToInt(transform.position.x / 10);
+        coordinates.y = Mathf.RoundToInt(transform.position.z / 10);
+        label.text = $"{coordinates.x}, {coordinates.y}";
+    }
+
+
+}
