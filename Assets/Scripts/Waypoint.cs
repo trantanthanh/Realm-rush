2b5ebf70a39a072f8e3799a1608554df0ec08399
Thành Trần Tấn <trantanthanh@gmail.com>
2024-03-04 02:41:37 +0700

    add bool isPlaceable for tile

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] bool isPlaceable;
+    void OnMouseDown()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (isPlaceable)
+        {
+            Debug.Log(transform.name);
+        }
     }
 }
