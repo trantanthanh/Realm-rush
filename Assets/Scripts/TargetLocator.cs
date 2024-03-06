64d20cd3a30da10eafa4789d83c53f46c7855fb5
Thành Trần Tấn <trantanthanh@gmail.com>
2024-03-07 03:17:53 +0700

    find the closest target and keep attack it when still in range attack

--- Assets/Scripts/TargetLocator.cs
+++ Assets/Scripts/TargetLocator.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class TargetLocator : MonoBehaviour
 {
     [SerializeField] Transform weapon;
-    Enemy target;
+    [SerializeField] float rangeAttack = 1f;
+    float distanceToTarget = -1f;
+    Enemy target = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,12 +24,25 @@ public class TargetLocator : MonoBehaviour
 
     private void FindClosestTarget()
     {
+        if (target != null && target.gameObject.activeInHierarchy)
+        {
+            distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
+            if (distanceToTarget < rangeAttack)
+            {
+                return;//continue attack old target in range.
+            }
+        }
         Enemy[] enemies = FindObjectsOfType<Enemy>();
         target = null;
         if (enemies.Length > 0)
         {
             Array.Sort(enemies, CompareDistance);
-            target = enemies[0];
+            float distance = Vector3.Distance(transform.position, enemies[0].transform.position);
+            if (distance <= rangeAttack)
+            {
+                distanceToTarget = distance;
+                target = enemies[0];
+            }
         }
     }
 
