72b5e67eb438e91eb3522302f46fd3a3ea69ccdd
Thành Trần Tấn <trantanthanh@gmail.com>
2024-03-19 15:41:35 +0700

    add build delay to tower

--- Assets/Scripts/Building/Tower.cs
+++ Assets/Scripts/Building/Tower.cs
@@ -6,6 +6,12 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] int price = 50;
+    [SerializeField] float buildDelay = 1f;
+
+    void Start()
+    {
+        
+    }
 
     public bool CreateTower(Tower towerPrefab, Vector3 position)
     {
@@ -18,5 +24,30 @@ public class Tower : MonoBehaviour
             return true;
         }
         return false;
+    }
+
+    IEnumerator Build()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+
+            foreach (Transform grandChild in child)
+            {
+                grandChild.gameObject.SetActive(false);
+            }
+        }
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+
+            yield return new WaitForSeconds(buildDelay);
+
+            foreach (Transform grandChild in child)
+            {
+                grandChild.gameObject.SetActive(true);
+            }
+        }
     }
 }
